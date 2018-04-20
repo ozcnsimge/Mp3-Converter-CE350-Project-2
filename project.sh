d50ba4fbba96a9331bed53789b25929f47ff959b
Simge Özcan <simgeozcan95@gmail.com>
2018-04-20 14:44:11 +0300

    Dialog boxes are added

--- project.sh
+++ project.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo list of audio files:
-
 for i in $(ls .)
 do
    if [ -n $(file $i | grep [aA]udio | awk 'BEGIN {FS=":"} {print $1}') ];
@@ -10,4 +8,20 @@ do
    fi
 done
 
-osascript osaex.scpt ${asd[@]}
+selectedItem=$(osascript osaex.scpt ${asd[@]})
+
+if [ $(echo $selectedItem | grep ogg$) ];
+then
+   echo you chose an ogg file
+   ffmpeg -i $selectedItem $selectedItem.wav
+   ffmpeg -i $selectedItem.wav -vn -ar 44100 -ac 2 -ab 192k -f mp3 $selectedItem.mp3
+elif [ $(echo $selectedItem | grep flac$) ];
+then
+   echo you chose a flac file
+   ffmpeg -i $selectedItem -ab 320k -map_metadata 0 -id3v2_version 3 $selectedItem.mp3
+elif [ $(echo $selectedItem | grep mp3$) ];
+then
+   echo you chose an mp3 file
+else
+   echo please choose a file
+fi
