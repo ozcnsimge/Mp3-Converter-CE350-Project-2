6fdb0497ef4f0519f3af78f04122467d8a2c1c1f
Simge Özcan <simgeozcan95@gmail.com>
2018-05-15 11:58:26 +0300

    ffmpeg verbose level adjusted

--- convert.sh
+++ convert.sh
@@ -2,14 +2,18 @@
 
 function convertFlacToMp3 {
 
+  dialog --title "Mp3 Converter" --infobox "Caonverting.. Please wait." 10 30
+
   #Crop the name of the file (without the extension).
 
   croppedName=$(echo $1 | awk -F\. '{print $1}')
 
-  ffmpeg -i $1 -y -b:a $2 -map_metadata 0 -id3v2_version 3 $croppedName.mp3
+  ffmpeg -i $1 -y -b:a $2 -map_metadata 0 -id3v2_version 3 $croppedName.mp3 2> /dev/null
 }
 
 function convertOggToMp3 {
+
+  dialog --title "Mp3 Converter" --infobox "Caonverting.. Please wait." 10 30
 
   #Crop the name of the file (without the extension).
 
@@ -17,10 +21,9 @@ function convertOggToMp3 {
 
   # If selected file is ogg, first decrypt to wav and the encryp to mp3.
 
-  ffmpeg -i $1 -y $croppedName.wav
+  ffmpeg -i $1 -y $croppedName.wav 2> /dev/null
 
-  ffmpeg -i $croppedName.wav -y -vn -ar 44100 -ac 2 -b:a $2 -f mp3 $croppedName.mp3
-  # FFREPORT=file=ffreport.log:level=32 ffmpeg -i input output
+  ffmpeg -i $croppedName.wav -y -vn -ar 44100 -ac 2 -b:a $2 -f mp3 $croppedName.mp3 2> /dev/null
 }
 
 
@@ -87,18 +90,19 @@ else
        dialog --title "Mp3 Converter" --msgbox "File successfully converted. Press <Enter> to exit" 10 30
      fi
 
+  # Display an info message if there is no audio file.
+
   elif [ -n "$exitVar" ]
   then
-    dialog --title "Mp3 Converter" --msgbox "No audio file found in this directory!" 10 30
+    dialog --title "Mp3 Converter" --msgbox "No audio file found in this directory! Press <Enter> to exit" 10 30
 
-  # Display an info box.
+  # Display a message box for cancelation.
 
-elif [ -z $selectedItem ]
+  elif [ -z $selectedItem ]
   then
+    dialog --title "Mp3 Converter" --msgbox "Action was canceled at your request. Press <Enter> to exit" 10 30
 
-    # Display an info message.
-
-    dialog --title "Mp3 Converter" --msgbox "Action was canceled at your request." 10 30
+  # Display a message box for unsupported file formats.
 
   else
     dialog --title "Mp3 Converter" --msgbox "This format is not supported. Press <Enter> to exit" 10 30
